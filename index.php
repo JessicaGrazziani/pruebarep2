a7f892dce474d41c230db9202fdeb9085e5d59c5
Jessica Grazziani <jeessicaa824@gmail.com>
2024-11-25 09:22:19 -0400

    probandotod

---
+++ index.php
@@ -0,0 +1,357 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
+
+</head>
+
+
+
+<body>
+
+
+    <!--Sección menú de navegación -->
+
+    <nav id="inicio" class="navbar navbar-expand-lg  navbar-dark  bg-dark">
+        <div class="container">
+
+
+            <a class="navbar-brand" href="#"> <i class="fas fa-utensils"></i> Restaurante</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="nav navbar-nav" ml-auto>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#inicio">Inicio </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#menu">Menu del dia</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#chefs">Chefs</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonios">Testimonio</a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#horario">Horarios</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacto">Contacto</a>
+                    </li>
+
+
+                </ul>
+
+
+            </div>
+
+        </div>
+    </nav>
+    <!--Sección banner-->
+    <section  class="container-fluid p-0">
+
+        <div class="banner-img" style="position:relative; background:url(images/1366_2000.jpg) center/cover no-repeat; height: 400px;">
+            <div class="banner-text" style="position:absolute; top:50%; left:50%; transform: translate(-50% - 50%); text-align:center; color:#fff;">
+                <h1>El Sabor</h1>
+                <P>Restaurante del mejor sabor casero</P>
+                <a href="#menu" class="btn btn-primary ">Ver Menu</a>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section id="id" class="container mt-4  text-center">
+        <div class="jumbotrom bg-dark text-white">
+            <br />
+            <h2>¡Bienvenid@ al Restaurante El Sabor!</h2>
+            <p> Descubre una experiencia culinaria única.</p>
+            <br />
+        </div>
+    </section>
+    <!--Sección chefst-->
+    <section id="chefs" class="container mt-4 text-center">
+        <h2>Nuestros Chefs</h2>
+        <div class="row">
+            <div class="col-md-4">
+
+                <div class="card">
+                    <img src="images/colaboradores/chef1.jpg" class="card-img-top" alt="chef 1" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 1</h5>
+                        <p class="card-text">El chef Marco.</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-4">
+
+                <div class="card">
+                    <img src="images/colaboradores/chef2.jpg" class="card-img-top" alt="chef 1" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 2</h5>
+                        <p class="card-text">El chef Renso.</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="col-md-4">
+
+                <div class="card">
+                    <img src="images/colaboradores/chef3.jpg" class="card-img-top" alt="chef 1" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Chef 3</h5>
+                        <p class="card-text">El chef Sofia.</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+    </section>
+    <!--Sección Testimonio-->
+    <section id="testimonios" class="bg-ligth py-5">
+
+        <div class="container">
+
+            <h2 class="text-center mb-4">Testimonios </h2>
+
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text"> Muy buena comida </p>
+
+                        </div>
+
+                        <div class="card-footer text-muted">
+
+                            Jessica Grazziani
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text"> Muy buena comida </p>
+
+                        </div>
+
+                        <div class="card-footer text-muted">
+
+                            Jessica Grazziani
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- Sección menú  comida-->
+    <section id="menu" class="container mt-4">
+        <h2 class="text-center"> Menú ( nuestra recomendación )</h2>
+        <br />
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col d-fex">
+                <div class="card">
+                    <img src="images/menu/menu1.jpg" class="card-img-top" alt="ensalada">
+                    <div class="card-body">
+                        <h5 class="card-title"> Ensalada de coliflor </h5>
+                        <p class="card-text small">
+                            <strong>Ingresnetes: </strong>Coliflor, lechuga, tomate, zanahoria, </strong>
+                        </p>
+                        <p class="card-text">
+                            <strong>$3.99</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col d-fex">
+                <div class="card">
+                    <img src="images/menu/menu2.jpg" class="card-img-top" alt="ensalada">
+                    <div class="card-body">
+                        <h5 class="card-title"> Sopa de Costilla</h5>
+                        <p class="card-text small">
+                            <strong>Ingresnetes: </strong>costilla, papa, ñame, jojoto</strong>
+                        </p>
+                        <p class="card-text">
+                            <strong>$2.99</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col d-fex">
+                <div class="card">
+                    <img src="images/menu/menu3.jpg" class="card-img-top" alt="ensalada">
+                    <div class="card-body">
+                        <h5 class="card-title"> Pasta con camarones</h5>
+                        <p class="card-text small">
+                            <strong>Ingresnetes: </strong>pasta, camarones, salsa pesto</strong>
+                        </p>
+                        <p class="card-text">
+                            <strong>$5.99</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col d-fex">
+                <div class="card">
+                    <img src="images/menu/menu4.jpg" class="card-img-top" alt="ensalada">
+                    <div class="card-body">
+                        <h5 class="card-title"> Tacos de pollo</h5>
+                        <p class="card-text small">
+                            <strong>Ingresnetes: </strong>tortilla, pollo, aguacate, tomate, queso</strong>
+                        </p>
+                        <p class="card-text">
+                            <strong>$4.99</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <br />
+    <br />
+
+
+
+    <!--Sección Contacto-->
+
+    <section  id="contacto" class="container mt-4">
+        <h2>Contacto</h2>
+        <p>Eslamos aquí para servirle, </p>
+        <form action="?" method="post">
+
+            <div class="mb-3">
+                <label for="name">Nombre:</label><br />
+                <input type="text" class="form-control" placeholder="Escribe tu nombre" required /><br />
+
+            </div>
+
+            <div class="mb-3">
+                <label for="email">Correo electrónico:</label><br />
+                <input type="email" class="form-control" name="correo" placeholder="Escribe tu  electronico" required><br />
+                <br />
+            </div>
+
+            <div class="mb-3">
+                <label for="message">Mensaje:</label><br />
+                <textarea rows="6" cols="50" id="mensaje" class="form-control" name="mensaje"></textarea><br />
+            </div>
+            <input type="submit" class="btn btn-primary" value="Enviar mensaje" />
+        </form>
+    </section>
+    <br /><br />
+
+
+    
+ <!--Sección de Horarios-->
+
+ <section id="horario" class="container mt-4">
+    <div class="text-center bg-light p-4">
+        <h3 class="mb-4"> Horario de atención </h3>
+        <div>
+            <p><strong> Lunes a Viernes </strong></p>
+            <p><strong> 11:00 a.am. - 10:00 pm. </strong></p>
+            
+        </div>
+
+        <div>
+            <p><strong> Sábados </strong></p>
+            <p><strong> 12:00 p.m - 5:00 pm. </strong></p>
+            
+        </div>
+
+        <div>
+            <p><strong> Domingo </strong></p>
+            <p><strong> 7:00 a.am. - 2:00 pm. </strong></p>
+            
+        </div>
+
+
+    </div>
+    </section>
+
+
+    <footer class="bg-dark text-light text-center py-3">
+        <!-- place footer here -->
+        <p>&copy; Copyright 2024 Restaurante el Sabor todos los derechos reservados. 1:35:23</p>
+    </footer>
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
